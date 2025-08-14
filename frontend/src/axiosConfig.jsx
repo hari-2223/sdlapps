12f4e2cbb0aa518d60bc50d336ce0ba3930e5e73
hari-2223 <hari94.mec@gmail.com>
2025-08-14 15:24:09 +1000

    Axios IP update

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.211.229.19:5001', // live
+  baseURL: 'http://3.107.29.62:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 

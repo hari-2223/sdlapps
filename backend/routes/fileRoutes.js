73a126df1b6f93b439c4900e20371eb8abf28f5c
hari-2223 <hari94.mec@gmail.com>
2025-08-15 03:53:54 +1000

    CFM-6 fileRoutes authmiddleware object error fix

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { uploadFile } = require('../controllers/fileController');
-const authMiddleware = require('../middleware/authMiddleware'); 
+const { protect } = require('../middleware/authMiddleware'); 
 const upload = require('../middleware/uploadMiddleware'); 
 
 // When POST request comes to /upload, this order
-router.post('/upload', authMiddleware, upload, uploadFile);
+router.post('/upload', protect, upload, uploadFile);
 
 module.exports = router;

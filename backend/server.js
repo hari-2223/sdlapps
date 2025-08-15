507d07b1728cf4043e9f460964d792edb4576fa7
hari-2223 <hari94.mec@gmail.com>
2025-08-15 21:38:37 +1000

    CFM-18 server.js code to configure express to show files

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,14 @@ dotenv.config();
 
 const app = express();
 const fileRoutes = require('./routes/fileRoutes');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/files', fileRoutes);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Export the app object for testing
 if (require.main === module) {

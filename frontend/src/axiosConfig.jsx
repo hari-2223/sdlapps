a1fb7fe146bf72ef4321feb712bb6dec46ddad20
hari-2223 <hari94.mec@gmail.com>
2025-08-17 06:34:32 +1000

    CFM-35 updated to version 1.0.3, ip address updated

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.106.222.124:5001', // live
+  baseURL: 'http://13.55.223.48:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 

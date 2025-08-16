d5832166913b37b564841888f84c8c0e852211ab
hari-2223 <hari94.mec@gmail.com>
2025-08-16 19:22:57 +1000

    CFM-24 added rename PUT route

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { uploadFile, getFiles } = require('../controllers/fileController');
+const { uploadFile, getFiles, renameFile } = require('../controllers/fileController');
 const { protect } = require('../middleware/authMiddleware'); 
 const upload = require('../middleware/uploadMiddleware'); 
 
 // When POST request comes to /upload, this order
 router.post('/upload', protect, upload, uploadFile);
 router.get('/', protect, getFiles);
+router.put('/:id', authMiddleware, renameFile);
 
 module.exports = router;
